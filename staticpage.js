fb32345ca40c62d4dbae13f71a1b3c9854c9c40c
[BintaSani] <[bintasani992@gmail.com]>
2023-09-09 10:07:13 +0100

    js update

--- staticpage.js
+++ staticpage.js
@@ -2,12 +2,27 @@
  let day = document.querySelector('[data-testid=currentDayOfTheWeek]');
  let time = document.querySelector('[data-testid=currentUTCTime]');
 
+const daysOfWeek= [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+]
+
+const newDate = new Date();
+const dayOfWeek = newDate.getDay();
+const name = daysOfWeek[dayOfWeek];
+
+day.textContent = `${name}.`;
 
  // functions
- let dayOfWeek = () => {
-     return new Date().toLocaleDateString('en-En', 
-     {'weekday': 'long'});
- }
+//  let dayOfWeek = () => {
+//      return new Date().toLocaleDateString('en-En', 
+//      {'weekday': 'long'});
+//  }
 
  let timeInMilliseconds = () => {
      const d = Date.now();
@@ -15,4 +30,4 @@
  };
  setInterval(timeInMilliseconds,1);
  // events
- day.textContent = dayOfWeek();
+ 
